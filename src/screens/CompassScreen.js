6258ce3430f8c69cb2fd92081a33e29be3870f28
THANGNDFPL <nguyenthang070103@gmail.com>
2023-05-25 06:47:05 +0700

    compassScreen

--- src/screens/CompassScreen.js
+++ src/screens/CompassScreen.js
@@ -12,42 +12,45 @@ export default function CompassScreen(props) {
                     Share your moment
                 </Text>
             </View>
-            <View style={{ position: 'absolute', left: 30, top: 150 }}>
+            <View style={{ position: 'absolute', left: 30, top: 120 }}>
                 <Text style={{ fontSize: 40, color: '#172432' }}>
                     Trip Planners
                 </Text>
                 <View style={{ borderColor: '#FF7757', borderBottomWidth: 3, width: "55%", top: 10, left: 10 }}></View>
 
             </View>
-            <View style={{ position: 'absolute', top: 230, left: 30, width: 322 }}>
+            <View style={{ position: 'absolute', top: 190, left: 30, width: 322 }}>
                 <Text style={{ fontSize: 18, }}>
                     20 years from now you will be more disappointed by the things that you didn’t do. Stop regretting and start travelling, start throwing off the bowlines.
                 </Text>
             </View>
-            <TouchableOpacity style={{ position: 'absolute', top: 360, left: 30, width: 322 }} activeOpacity={0.8}>
+            <TouchableOpacity style={{ position: 'absolute', top: 300, left: 30, width: 322 }} activeOpacity={0.8}>
                 <View style={{ backgroundColor: '#172432', width: 43, height: 43 }}></View>
                 <View style={{ top: 12, left: "55%", backgroundColor: '#767E86', width: 43, height: 43 }}></View>
                 <View style={{ top: -66, left: 12, backgroundColor: "#FF7757", width: 186, height: 61, borderRadius: 12, alignItems: 'center', justifyContent: 'center' }}>
                     <Text style={{ fontSize: 18, color: 'white' }}>View all trip plans</Text>
                 </View>
             </TouchableOpacity>
-            <View style={{ left: 0, position: 'absolute', top: 460 }}>
+            <View style={{ left: 20, position: 'absolute', top: 400 }}>
                 <Text style={{ fontSize: 40, lineHeight: 52 }}>
                     Destination Gallery
                 </Text>
             </View>
-            <View style={{ borderColor: '#FF7757', borderBottomWidth: 3, width: "100%", top: 520,position:'absolute' }}></View>
-            <View style={{ position: 'absolute', top: 540, left: 0 }}>
+            <View style={{ borderColor: '#FF7757', borderBottomWidth: 3, width: "90%", top: 450,position:'absolute' ,left:20}}></View>
+            <View style={{ position: 'absolute', top: 460, left: 20 }}>
                 <Text style={{ color: "#767E86", fontSize: 18 }}>
                     Our photo gallery on trip
                 </Text>
             </View>
-            <View style={{position:'absolute',top:560,width: '80%'}}>
+            <View style={{position:'absolute',top:470,width: '80%'}}>
                 <Image style={{ width: '100%', height: 200 ,borderRadius:26,top:26}} resizeMode="center" source={require('../img/photo7.png')} />
             </View>
-            <View>
-                <TouchableOpacity>
-                    
+            <View style={{position:'absolute',top:720,flexDirection:'row',columnGap:40,alignItems:'center'}} >
+                <TouchableOpacity style={{backgroundColor:"#172432",padding:8,borderRadius:12,alignItems:'center',justifyContent:'center'}} activeOpacity={0.7}>
+                    <Ionicons name='chevron-back-outline' color="white" size={26}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={{backgroundColor:"#FF7757",padding:8,borderRadius:12,alignItems:'center',justifyContent:'center'}} activeOpacity={0.7}>
+                    <Ionicons name='chevron-forward-outline' color="white" size={26}/>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
